35af11c249a4beb8beb92bc7ee402860b504aba4
abdisamadovich <islombobokulov143@gmail.com>
2023-10-25 17:06:44 +0500

    DataAccess : Create IGetAll and ISearchable

---
+++ src/Products.DataAccess/Common/Interfaces/ISearchable.cs
@@ -0,0 +1,9 @@
+﻿using Products.DataAccess.Utils;
+
+namespace Products.DataAccess.Common.Interfaces;
+
+public interface ISearchable<TModel>
+{
+    public Task<(int ItemsCount, IList<TModel>)> Searchable(string search,
+        PaginationParams @params);
+}
